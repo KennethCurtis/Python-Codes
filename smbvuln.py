02708589dae59303b963202a9b716a1c8d9e271a
KennethCurtis <107327050+KennethCurtis@users.noreply.github.com>
2022-09-03 13:01:22 -0400

    Update smbvuln.py

--- smbvuln.py
+++ smbvuln.py
@@ -1,5 +1,4 @@
-#Results from the scans will indicate just host up if it returns negative, but will state Vulnerable if targetable.
-#This will create a directory in linux for the particular IP, and then create a total output file and vulnerability summary .txts
+#This will create a directory in linux for the particular IP, and then create a raw and pretty format of the output of the nmap scans
 #version 1.0
 
 import argparse
@@ -8,38 +7,44 @@ import subprocess
 from subprocess import run
 
 def startup():
-    vuln_list = ['smb-vuln-cve2009-3103.nse',
-                 'smb-vuln-ms08-067.nse',
-                 'smb-vuln-regsvc-dos.nse',
-                 'smb-vuln-cve-2017-7494.nse',
-                 'smb-vuln-ms10-054.nse',
-                 'smb-vuln-webexec.nse',
-                 'smb-vuln-ms06-025.nse',
-                 'smb-vuln-ms10-061.nse',
-                 'smb2-vuln-uptime.nse',
-                 'smb-vuln-conficker.nse',
-                 'smb-vuln-ms07-029.nse',
-                 'smb-vuln-ms17-010.nse']
+    vuln_list = ['smb-protocols',
+                 'smb-security-mode',
+                 'smb-enum-sessions',
+                 'smb-enum-shares',
+                 'smb-enum-domains',
+                 'smb-enum-groups',
+                 'smb-enum-shares,smb-ls',
+                 'smb-server-stats',
+                 'smb-os-discovery']
     parser = argparse.ArgumentParser()
     parser.add_argument('IP', type=str, help="Enter the value of the IP of the target.")
     args = parser.parse_args()
     os.system('mkdir ' + args.IP)
-    doc = open('/home//python/scanners/' + args.IP + '/smbvuln.txt', 'a+')
-    doc1 = open('/home/python/scanners/' + args.IP + '/smbnmap.txt', 'a+')
+    doc = open('/home/kali/Desktop/python/scanners/' + args.IP + '/smbenumnmap.txt', 'a+')
     for i in vuln_list:
         payload = 'nmap ' +args.IP + ' --script ' + i + ' -p 445'
         output = subprocess.Popen(['nmap', args.IP, '--script', i, '-p 445'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         stdout= output.communicate()
         print(stdout)
-        check_vuln(stdout, doc, doc1, i)
+        check_vuln(stdout, doc, i)
+    doc.close()
+    prettify(args)
+    doc.close()
+    
 
+def check_vuln(stdout, doc, i):
+    doc.write (str(stdout))
+    doc.write('\n')
 
-def check_vuln(stdout, doc, doc1, i):
-    if 'VULNERABLE' in str(stdout):
-        doc.write (i + ': VULERNABLE\n')
-    else:
-        doc.write (i + ': not a valid exploit\n')
-    doc1.write (str(stdout))
-    doc1.write('\n')
+def prettify(args):
+    doc = open('/home/kali/Desktop/python/scanners/' + args.IP + '/smbenumnmap.txt', 'r')
+    doc1=open('/home/kali/Desktop/python/scanners/' + args.IP + '/pretty_smb_nmap.txt', 'w+')
+    read_output = doc.read()
+    double_remove = read_output.replace('\\n\\n','\n')
+    csv_replace = double_remove.replace('\\n',',')
+    csv_list = csv_replace.split (',')
+    for i in csv_list:
+        doc1.write (i + '\n')
+    doc1.close()
 
 startup()
